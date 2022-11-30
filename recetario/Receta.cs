8811aceb2ce6140b9b8e6ce439cb1b7a5702583d
arcznze <dlmj.2211@gmail.com>
2022-11-29 20:32:15 -0400

    class added.

--- recetario/Receta.cs
+++ recetario/Receta.cs
@@ -8,6 +8,50 @@ using Antlr4.Runtime.Misc;
 
 namespace recetario
 {
+    public class recipes
+    {
+        public int id;
+        public string name = "";
+        public int portions;
+        public int? prep_time; //null
+        public string? prep_time_unit = ""; //null
+        public int? cook_time; //null
+        public string? cook_time_unit = ""; //null
+        public int calories;
+    }
+
+    public class ingredients
+    {
+        public ingredients(int id, string name, float quantity) => (this.id, this.name, this.quantity) = (id, name, quantity);
+        public int id;
+        public int? recipe_id;
+        public string name = "";
+        public float quantity;
+        public int? unit_id; //null
+
+        public string ObtenerInsert()
+        {
+            return $"INSERT INTO ingredients (id, recipe_id, name, quantity, unit_id) " +
+                $"VALUES ({this.id}, {this.recipe_id},{this.name}, {this.quantity}, {this.unit_id})";
+        }
+
+
+    }
+
+    public class ingredient_units
+    {
+        public int id;
+        public string name = "";
+    }
+
+    public class cooking_steps
+    {
+        public int id;
+        public int recipe_id;
+        public int step_number;
+        public string description = "";
+    }
+
     internal class Receta
     {
 
